c6c588666343daa35a37fee6d7d8466a43673449
Chirabrata Ghosal <chirabrataghosal9@gmail.com>
2026-09-09 14:08:55 +0530

    feat: comment out Wi-Fi and HTTPClient code in ps128_iot.ino; update package.json for prisma generate postinstall script

--- esp32/ps128_iot.ino
+++ esp32/ps128_iot.ino
@@ -1,71 +1,71 @@
-#include <WiFi.h>
-#include <HTTPClient.h>
+// #include <WiFi.h>
+// #include <HTTPClient.h>
 
-const char* ssid = "Chirabrata";
-const char* password = "chirabrata";
-const char* serverUrl = "https://ps128-livestock-api.onrender.com/api/analyze";
+// const char* ssid = "Chirabrata";
+// const char* password = "chirabrata";
+// const char* serverUrl = "https://ps128-livestock-api.onrender.com/api/analyze";
 
-void setup() {
-  Serial.begin(115200);
-  delay(500);
+// void setup() {
+//   Serial.begin(115200);
+//   delay(500);
 
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to Wi-Fi");
+//   WiFi.begin(ssid, password);
+//   Serial.print("Connecting to Wi-Fi");
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+//   while (WiFi.status() != WL_CONNECTED) {
+//     delay(500);
+//     Serial.print(".");
+//   }
 
-  Serial.println();
-  Serial.println("Wi-Fi connected");
-  Serial.print("ESP32 IP: ");
-  Serial.println(WiFi.localIP());
+//   Serial.println();
+//   Serial.println("Wi-Fi connected");
+//   Serial.print("ESP32 IP: ");
+//   Serial.println(WiFi.localIP());
 
-  randomSeed(micros());
-}
+//   randomSeed(micros());
+// }
 
-void loop() {
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("Wi-Fi disconnected. Reconnecting...");
-    WiFi.reconnect();
-    delay(5000);
-    return;
-  }
+// void loop() {
+//   if (WiFi.status() != WL_CONNECTED) {
+//     Serial.println("Wi-Fi disconnected. Reconnecting...");
+//     WiFi.reconnect();
+//     delay(5000);
+//     return;
+//   }
 
-  float temperature = 38.5 + (random(0, 20) / 10.0);
-  int activity = random(20, 80);
+//   float temperature = 38.5 + (random(0, 20) / 10.0);
+//   int activity = random(20, 80);
 
-  String payload = "{";
-  payload += "\"health_report\":{";
-  payload += "\"animal\":\"Cow\",\"symptoms\":[\"Fever\"]";
-  payload += "},";
-  payload += "\"iot_telemetry\":{";
-  payload += "\"animal_id\":\"ESP32-01\",";
-  payload += "\"temperature\":" + String(temperature, 1) + ",";
-  payload += "\"activity\":" + String(activity);
-  payload += "}";
-  payload += "}";
+//   String payload = "{";
+//   payload += "\"health_report\":{";
+//   payload += "\"animal\":\"Cow\",\"symptoms\":[\"Fever\"]";
+//   payload += "},";
+//   payload += "\"iot_telemetry\":{";
+//   payload += "\"animal_id\":\"ESP32-01\",";
+//   payload += "\"temperature\":" + String(temperature, 1) + ",";
+//   payload += "\"activity\":" + String(activity);
+//   payload += "}";
+//   payload += "}";
 
-  HTTPClient http;
-  http.begin(serverUrl);
-  http.addHeader("Content-Type", "application/json");
+//   HTTPClient http;
+//   http.begin(serverUrl);
+//   http.addHeader("Content-Type", "application/json");
 
-  Serial.println("Sending payload:");
-  Serial.println(payload);
+//   Serial.println("Sending payload:");
+//   Serial.println(payload);
 
-  int httpCode = http.POST(payload);
-  Serial.print("Response code: ");
-  Serial.println(httpCode);
+//   int httpCode = http.POST(payload);
+//   Serial.print("Response code: ");
+//   Serial.println(httpCode);
 
-  if (httpCode > 0) {
-    Serial.println("Response body:");
-    Serial.println(http.getString());
-  } else {
-    Serial.print("Request failed: ");
-    Serial.println(http.errorToString(httpCode));
-  }
+//   if (httpCode > 0) {
+//     Serial.println("Response body:");
+//     Serial.println(http.getString());
+//   } else {
+//     Serial.print("Request failed: ");
+//     Serial.println(http.errorToString(httpCode));
+//   }
 
-  http.end();
-  delay(15000);
-}
+//   http.end();
+//   delay(15000);
+// }
